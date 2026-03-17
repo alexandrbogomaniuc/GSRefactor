c674fa4b2ea3eacdbda0002aa43b41ef0e6c7338
Alex B <alexb@Alexs-MacBook-Air.local>
2026-03-17 18:42:07 +0000

    refactor(rename): clean legacy promo javadocs in responses

--- gs-server/common-promo/src/main/java/com/dgphoenix/casino/common/promo/messages/server/responses/NotificationShownResponse.java
+++ gs-server/common-promo/src/main/java/com/dgphoenix/casino/common/promo/messages/server/responses/NotificationShownResponse.java
@@ -3,7 +3,7 @@ package com.abs.casino.common.promo.messages.server.responses;
 import com.abs.casino.gs.singlegames.tools.cbservtools.response.ServerResponse;
 
 /**
- * @deprecated It is the old version of {@link com.dgphoenix.casino.common.promo.messages.server.responses.NotificationsShownResponse}
+ * @deprecated It is the old version of {@link com.abs.casino.common.promo.messages.server.responses.NotificationsShownResponse}
  * Left only for backward capability. Should be removed in near future.
  */
 public class NotificationShownResponse extends ServerResponse {
