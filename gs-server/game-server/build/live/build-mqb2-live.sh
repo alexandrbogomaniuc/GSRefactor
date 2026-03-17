bf60d7c5d894c74bc45ffb3576a867222b85fb48
Alex B <alexb@Alexs-MacBook-Air.local>
2026-03-17 15:16:59 +0000

    refactor(rename): remove approved token residue in game-server build scripts

--- gs-server/game-server/build/live/build-mqb2-live.sh
+++ gs-server/game-server/build/live/build-mqb2-live.sh
@@ -3,6 +3,6 @@
 BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../";
 
 PROPERTIES_FILE="live/mqb2-live.properties";
-BRAND="betsoft";
+BRAND="abs";
 
 source "${BUILD_DIR}/build-server.sh"
